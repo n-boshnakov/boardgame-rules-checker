4c29448b11339ac13ec2246d4ef6f9e51d2353fa
Nikolay Boshnakov <nikolay.boshnakov@sap.com>
2026-01-30 10:03:22 +0200

    Cleaned up the repo (#5)

--- check_indices.py
+++
@@ -1,9 +0,0 @@
-from elasticsearch import Elasticsearch
-
-es = Elasticsearch(['http://localhost:9200'])
-indices = es.cat.indices(format='json')
-
-print('Available indices:')
-for idx in indices:
-    if not idx['index'].startswith('.'):
-        print(f"  - {idx['index']} ({idx['docs.count']} docs)")
